6bcf46b1ddc4b826a591545c8fa68712e9ec3e5e
Your Name <nuttkit>
2021-03-04 20:08:44 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <img src="https://www.prachachat.net/wp-content/uploads/2018/09/%E0%B8%81.%E0%B8%A5.%E0%B8%95..jpeg" class="rounded mx-auto d-block" width="300">
+    <div class="container">
+      <h3 class="text-center">รายชื่อ บริษัทหลักทรัพย์จัดการกองทุนรวมจำกัด</h3>
+      <table class="table table-striped table-dark">
+        <thead>
+          <tr>
+           <th scope="col">รหัสนิติบุคคล</th>
+           <th scope="col">ชื่อนิติบุคคล/บุคคล (ภาษาไทย)</th>
+           <th scope="col">ชื่อนิติบุคคล/บุคคล (ภาษาอังกฤษ)</th>
+           <th scope="col">แก้ไขล่าสุด</th>
+          </tr>
+        </thead>
+        <tr v-for="user in users" :key="user.unique_id">
+          <td>{{user.unique_id}}</td>
+          <td>{{user.name_th}}</td>
+          <td>{{user.name_en}}</td>
+          <td>{{user.last_upd_date}}</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from"axios"; 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  mounted(){
+    this.getUser();
+  },
+  methods:{
+    getUser(){
+      axios.get("https://api.sec.or.th/FundFactsheet/fund/amc?key=8888dd4340e34b889d35e3529895224c")
+      .then(response=>{
+      this.users=response.data;
+      });
+    }
+  },
+  data(){
+    return{
+      users:[],
+      user:{
+        unique_id:"",
+        name_th:"",
+        name_en:"",
+        last_upd_date:""
+      }
+
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
